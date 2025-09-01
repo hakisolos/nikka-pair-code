845c8464994f8d4df3df87ac7537236201125ad1
hakisolos <165590443+hakisolos@users.noreply.github.com>
2025-09-01 08:15:22 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,9 @@ export default {
   PREFIX: process.env.PREFIX || "X",
   EMAIL: process.env.EMAIL || "ironman@onlyfans.com",
   PASS: process.env.PASS || "Kimi da yo kimi nandayo..",
-  MESSAGE: process.env.MESSAGE || "Thank you for using our bot, don’t share your session ID 😤",
+  MESSAGE: process.env.MESSAGE || "CONNECTED: Do NOT share your session url",
   IMAGE: process.env.IMAGE || "https://cdn.ironman.my.id/i/2iceb4.jpeg",
   DBURL: process.env.DBURL || '',
   SUPKEY: process.env.SUPKEY ||  ''
 };
+
